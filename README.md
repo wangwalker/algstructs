7955e8b6e5ce6d3985bebcc81624db653e7e8661
Walkerant <330722864@qq.com>
2023-12-14 16:30:50 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Algstructs
+
+A description of this package.
---
+++ Tests/AlgstructsTests/AlgstructsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Algstructs
+
+final class AlgstructsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Algstructs().text, "Hello, World!")
+    }
+}
